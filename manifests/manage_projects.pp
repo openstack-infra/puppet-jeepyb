47a09055d5d22dcdaa70a5afa372f73222bd1549
Yolanda Robla <yolanda.robla-mota@hp.com>
2015-04-08 11:40:18 +0200

    Add helpers to execute manage_projects
    Manage_projects is run in two different ways:
    one in a cronjob, one due to subscription. Create
    helper classes to isolate that behaviour, to move
    logic from system-config.
    
    Change-Id: I4132646446dfe575c333b981aea9a7778c13bcf0

---
+++ manifests/manage_projects.pp
@@ -0,0 +1,38 @@
+# == Class: jeepyb::manage_projects
+
+class jeepyb::manage_projects(
+  $timeout = 900, # 15 minutes
+  $sub     = [],
+  $req     = [],
+  $logfile = '/var/log/manage_projects.log',
+  $log_options = [
+    'compress',
+    'missingok',
+    'rotate 30',
+    'daily',
+    'notifempty',
+    'copytruncate',
+  ],
+) {
+  validate_array($sub)
+  validate_array($req)
+  validate_array($log_options)
+
+  include jeepyb
+
+  exec { 'jeepyb_manage_projects':
+    command     => "/usr/local/bin/manage-projects -v >> ${logfile} 2>&1",
+    timeout     => $timeout, # 15 minutes
+    subscribe   => $sub,
+    refreshonly => true,
+    logoutput   => true,
+    require     => $req,
+  }
+
+  include logrotate
+  logrotate::file { $logfile:
+    log     => $logfile,
+    options => $log_options
+    require => Exec['jeepyb_manage_projects'],
+  }
+}
