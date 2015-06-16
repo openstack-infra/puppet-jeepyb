4db6f2068a03b48fd146eba7e1b0e2e33ce45c65
Jeremy Stanley <fungi@yuggoth.org>
2015-06-16 13:15:59 +0000

    Separate the fetch remotes log dir and file
    The defaults for jeepyb::fetch_remotes were impossible, since the
    cron wanted to run as an arbitrary user and create a file in
    /var/log by default. Instead have it own a subdirectory where it
    has write permission and put the log there.
    
    Co-Authored-By: Yolanda Robla <info@ysoft.biz>
    
    Depends-On: I341290c430023e5e9a24bf2247482ed818ea4a26
    Depends-On: Ifd052804a24806f8f660d25dca68c5af9c4fb605
    Change-Id: Ic59525fd44611858094a939961512c328b05831d

--- manifests/manage_projects.pp
+++ manifests/manage_projects.pp
@@ -1,8 +1,10 @@
 # == Class: jeepyb::manage_projects
 
 class jeepyb::manage_projects(
+  $ensure = present,
   $timeout = 900, # 15 minutes
-  $logfile = '/var/log/manage_projects.log',
+  $logfile = 'manage_projects.log',
+  $user = 'gerrit2',
   $log_options = [
     'compress',
     'missingok',
@@ -13,20 +15,38 @@ class jeepyb::manage_projects(
   ],
 ) {
   validate_array($log_options)
+  $logdir = '/var/log/jeepyb'
 
   include ::jeepyb
+
+  if !defined(File[$logdir]) {
+    file { $logdir:
+      ensure  => directory,
+      mode    => '0755',
+      owner   => $user,
+      require => User[$user],
+    }
+  }
 
   exec { 'jeepyb_manage_projects':
     command     => "/usr/local/bin/manage-projects -v >> ${logfile} 2>&1",
     timeout     => $timeout, # 15 minutes
     refreshonly => true,
     logoutput   => true,
+    require     => File[$logdir],
   }
 
   include ::logrotate
   logrotate::file { $logfile:
-    log     => $logfile,
+    ensure  => $ensure,
+    log     => "${logdir}/${logfile}",
     options => $log_options,
     require => Exec['jeepyb_manage_projects'],
   }
+
+  # clean up buggy files
+  logrotate::fileremoval {
+    '/var/log/manage_projects.log':
+  }
+
 }
